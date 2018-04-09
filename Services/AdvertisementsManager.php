f463178710c56d4f0792edbe9042dc2f2edf71c7
Jérémie Mosset-Chafik <jeremie.mossetchafik@gmail.com>
2018-04-09 16:33:48 +0200

    added footer and header

--- Services/AdvertisementsManager.php
+++ Services/AdvertisementsManager.php
@@ -72,25 +72,37 @@ class AdvertisementsManager
     {
         $htmlContent = str_replace(
             '<!-- IsSubscribedBegin -->',
-            "<%if(targetData.abonne=='1'){%><!-- IsSubscribedBegin -->",
+            "<% if(targetData.abonne== '1'){ %><!-- IsSubscribedBegin -->",
             $htmlContent
         );
 
         $htmlContent = str_replace(
             '<!-- IsNotSubscribedBegin -->',
-            "<%if(targetData.abonne=='2'){%><!-- IsNotSubscribedBegin -->",
+            "<% if(targetData.abonne== '2'){ %><!-- IsNotSubscribedBegin -->",
             $htmlContent
         );
 
         $htmlContent = str_replace(
             '<!-- IsSubscribedEnd -->',
-            "<!-- IsSubscribedEnd --><%}%>",
+            "<!-- IsSubscribedEnd --><% } %>",
             $htmlContent
         );
 
         $htmlContent = str_replace(
             '<!-- IsNotSubscribedEnd -->',
-            "<!-- IsNotSubscribedEnd --><%}%>",
+            "<!-- IsNotSubscribedEnd --><% } %>",
+            $htmlContent
+        );
+
+        $htmlContent = str_replace(
+            '<!-- insertMirrorPage -->',
+            '<%@ include view="bay_NewsAuto_MirrorPage"%>',
+            $htmlContent
+        );
+
+        $htmlContent = str_replace(
+            '<!-- insertMirrorPage -->',
+            '<%@ include view="bay_NewsAuto_Unsub_CrxAlert" %>;',
             $htmlContent
         );
 
@@ -120,23 +132,13 @@ class AdvertisementsManager
         $htmlContent = $crawler->saveHTML();
 
         foreach ($advertisementEntities as $ad) {
-            switch ($ad->getTarget()) {
-                case 'subscribers':
-                    $selector = '.ad-subscriber';
-                    break;
-                case 'not_subscribers':
-                    $selector = '.ad-not-subscriber';
-                    break;
-                default:
-                    $selector = '.ad-all';
-            }
             if ($logger !== null) {
                 $logger->info('[' . date(DATE_ISO8601) . '] Advertisement at position ' . $ad->getPosition());
             }
             if ($crawler->filter('.ad-' . $ad->getPosition())->count() > 0) {
-                $htmlContent = $this->insertAdvertisement($ad, $htmlContent, $ad->getPosition(), $selector, $isForNeolane);
+                $htmlContent = $this->insertAdvertisement($ad, $htmlContent, $ad->getPosition(), $isForNeolane);
             } else {
-                $htmlContent = $this->insertAdvertisement($ad, $htmlContent, $newsletterEntity->getNbPositions(), $selector, $isForNeolane);
+                $htmlContent = $this->insertAdvertisement($ad, $htmlContent, $newsletterEntity->getNbPositions(), $isForNeolane);
             }
         }
 
@@ -164,27 +166,26 @@ class AdvertisementsManager
      * @param $isForNeolane
      * @return string
      */
-    public function insertAdvertisement($ad, $htmlContent, $position, $targetSelector, $isForNeolane)
+    public function insertAdvertisement($ad, $htmlContent, $position, $isForNeolane)
     {
-        $adContent = "
-            <table class=\"row advertisement hiddenAd ad-1 no-padding-left no-padding-right\" style=\"border-collapse: collapse;border-spacing: 0;display: table;padding: 0;padding-left: 0!important;padding-right: 0!important;position: relative;text-align: left;vertical-align: top;width: 100%;\">
-            <tbody style=\"padding-left:0!important;padding-right:0!important\"><tr style=\"padding:0;padding-left:0!important;padding-right:0!important;text-align:left;vertical-align:top\">
-            <th class=\"small-12 large-8 columns first last\" style=\"Margin:0 auto;color:#0a0a0a;font-family:Helvetica,Arial,sans-serif;font-size:16px;font-weight:400;line-height:1.3;margin:0 auto;padding:0;padding-bottom:16px;padding-left:0!important;padding-right:0!important;text-align:left;width:370.67px\">
-            <table style=\"border-collapse:collapse;border-spacing:0;padding:0;padding-left:0!important;padding-right:0!important;text-align:left;vertical-align:top;width:100%\">
-            <tbody><tr style=\"padding:0;padding-left:0!important;padding-right:0!important;text-align:left;vertical-align:top\">
-            <th style=\"Margin:0;color:#0a0a0a;font-family:Helvetica,Arial,sans-serif;font-size:16px;font-weight:400;line-height:1.3;margin:0;padding:0;padding-left:0!important;padding-right:0!important;text-align:left\">
-            <a style=\"Margin:0;color:#2199e8;font-family:Helvetica,Arial,sans-serif;font-weight:400;line-height:1.3;margin:0;padding:0;padding-left:0!important;padding-right:0!important;text-align:left;text-decoration:none\" href=\"" . $ad->getRedirectURL() . "\">
-            <img alt=\"\" style=\"-ms-interpolation-mode:bicubic;border:none;clear:both;display:block;max-width:100%;outline:0;padding-left:0!important;padding-right:0!important;text-decoration:none;width:auto\" src=\"" . $ad->getImageLink() . "\">
-            </a></th></tr></tbody></table></th></tr></tbody></table>
-            ";
+        $adContent =
+            "<table class=\"row advertisement hiddenAd ad-1 no-padding-left no-padding-right\" style=\"border-collapse: collapse;border-spacing: 0;display: table;padding: 0;padding-left: 0!important;padding-right: 0!important;position: relative;text-align: left;vertical-align: top;width: 100%;\">" .
+            "<tbody style=\"padding-left:0!important;padding-right:0!important\"><tr style=\"padding:0;padding-left:0!important;padding-right:0!important;text-align:left;vertical-align:top\">" .
+            "<th class=\"small-12 large-8 columns first last\" style=\"Margin:0 auto;color:#0a0a0a;font-family:Helvetica,Arial,sans-serif;font-size:16px;font-weight:400;line-height:1.3;margin:0 auto;padding:0;padding-bottom:16px;padding-left:0!important;padding-right:0!important;text-align:left;width:370.67px\">" .
+            "<table style=\"border-collapse:collapse;border-spacing:0;padding:0;padding-left:0!important;padding-right:0!important;text-align:left;vertical-align:top;width:100%\">" .
+            "<tbody><tr style=\"padding:0;padding-left:0!important;padding-right:0!important;text-align:left;vertical-align:top\">" .
+            "<th style=\"Margin:0;color:#0a0a0a;font-family:Helvetica,Arial,sans-serif;font-size:16px;font-weight:400;line-height:1.3;margin:0;padding:0;padding-left:0!important;padding-right:0!important;text-align:left\">" .
+            "<a style=\"Margin:0;color:#2199e8;font-family:Helvetica,Arial,sans-serif;font-weight:400;line-height:1.3;margin:0;padding:0;padding-left:0!important;padding-right:0!important;text-align:left;text-decoration:none\" href=\"" . $ad->getRedirectURL() . "\">" .
+            "<img alt=\"\" style=\"-ms-interpolation-mode:bicubic;border:none;clear:both;display:block;max-width:100%;outline:0;padding-left:0!important;padding-right:0!important;text-decoration:none;width:auto\" src=\"" . $ad->getImageLink() . "\">".
+            "</a></th></tr></tbody></table></th></tr></tbody></table>";
 
         if ($isForNeolane) {
             switch ($ad->getTarget()) {
                 case 'subscribers':
-                    $adContent = '<% if(targetData.abonne==\'1\'){ %>' . $adContent . '<% } %>';
+                    $adContent = '<% if(targetData.abonne == \'1\') { %>' . $adContent . '<% } %>';
                     break;
                 case 'not_subscribers':
-                    $adContent = '<% if(targetData.abonne==\'2\'){ %>' . $adContent . '<% } %>';
+                    $adContent = '<% if(targetData.abonne == \'2\') { %>' . $adContent . '<% } %>';
                     break;
             }
         }
