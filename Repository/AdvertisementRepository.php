e0e6a61fbcff5bc52f34889194ec56dd221e00cb
Unknown <jeremie.mossetchafik@gmail.com>
2018-06-01 13:37:10 +0200

    Added way to retrieve an advertisement by its date and

--- Repository/AdvertisementRepository.php
+++ Repository/AdvertisementRepository.php
@@ -18,12 +18,13 @@ class AdvertisementRepository extends EntityRepository
     /**
      * @param $page
      * @param $numberPerPage
+     * @param array $sortArray
+     * @param $filter
      * @return Paginator
      */
     public function getAdvertisements($page, $numberPerPage, $sortArray = [], $filter)
     {
         $qb = $this->getEntityManager()->createQueryBuilder();
-        $condition = null;
 
         $query = $qb->select('a')
             ->from('BayardNewsletterORMBundle:Advertisement', 'a');
@@ -51,7 +52,7 @@ class AdvertisementRepository extends EntityRepository
                 $query->leftJoin('a.dates', 'd')
                     ->leftJoin('a.newsletterTypes', 'nt')
                     ->leftJoin('a.newsletterTypes', 'nt2')->addSelect('nt2')
-                    ->where($qb->expr()->eq('nt.name', ':type')                    )
+                    ->where($qb->expr()->eq('nt.name', ':type'))
                     ->setParameter('type', $filter);
                 break;
             case 'all':
@@ -75,5 +76,30 @@ class AdvertisementRepository extends EntityRepository
 
         return new Paginator($query, true);
 
+    }
+
+    /**
+     * @param \DateTime $date
+     * @param $type
+     * @return array
+     */
+    public function getAdvertisementByDateAndType(\DateTime $date, $type)
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+
+        $query = $qb->select('a')
+            ->from('BayardNewsletterORMBundle:Advertisement', 'a');
+
+        $date->setTime(0, 0, 0);
+
+
+        $query->innerJoin('a.dates', 'd')
+            ->innerJoin('a.newsletterTypes', 'nt')
+            ->where(':date BETWEEN d.beginningDate AND d.endDate')
+            ->andWhere('nt.name = :type')
+            ->setParameter('type', $type)
+            ->setParameter('date', $date);
+
+        return $query->getQuery()->getResult();
     }
 }
